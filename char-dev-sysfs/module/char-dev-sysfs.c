f33202abcd237f45b4b4f6b61352d07099e9b30e
JS.Maeng <maeng@telechips.com>
2018-12-20 15:14:34 +0900

    Add char-dev-sysfs example
    Change-Id: I54cf71a5d504b3061f6bfaac0e5f8dbf2774fbe0

---
+++ char-dev-sysfs/module/char-dev-sysfs.c
@@ -0,0 +1,126 @@
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/init.h>		/* Needed for the macros */
+#include <linux/fs.h>
+#include <asm/errno.h>		/* Needed for error code */
+#include <linux/cdev.h>		/* Needed for using cdev */
+#include <linux/device.h>	/* Needed for class create */
+
+
+/* Prototypes */
+static long char_dev_sysfs_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
+static int char_dev_sysfs_open(struct inode *inode, struct file *file);
+static int char_dev_sysfs_release(struct inode *inode, struct file *file);
+
+#define DEVICE_NAME "char-dev-sysfs" /* Dev name about '/proc/devices' */
+
+/* Global variables */
+static int char_dev_sysfs_device_open = 0;	/* to prevent multiple access to the device */
+static dev_t first;	/* Global variable for the first device number */
+static struct cdev* char_dev_sysfs_cdev;
+static struct class* char_dev_sysfs_class;
+
+
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.unlocked_ioctl = char_dev_sysfs_ioctl,
+	.open = char_dev_sysfs_open,
+	.release = char_dev_sysfs_release
+};
+
+static long char_dev_sysfs_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+	printk(KERN_INFO "device_ioctl \n");
+	
+	return 0;
+}
+
+static int char_dev_sysfs_open(struct inode *inode, struct file *file)
+{
+	if(char_dev_sysfs_device_open) return -EBUSY;
+	
+	char_dev_sysfs_device_open++;
+	
+	printk(KERN_INFO "device_open \n");
+	
+	return 0;
+}
+static int char_dev_sysfs_release(struct inode *inode, struct file *file)
+{
+	char_dev_sysfs_device_open--;
+	
+	printk(KERN_INFO "device_release \n");
+	
+	return 0;
+}
+static int __init char_dev_sysfs_init(void)
+{
+	int retval;
+	printk(KERN_INFO "module init \n");
+
+	retval = alloc_chrdev_region(&first, 0,1, DEVICE_NAME);
+	if(retval < 0) {
+		printk("alloc_chrdev_region() failed!\n");
+		return -EBUSY;
+	}
+
+	printk("\n Major No: %d Minor_no : %d \n", MAJOR(first), MINOR(first));
+
+	// argument name is class sub-dir as '/sys/class/char-dev'
+	char_dev_sysfs_class = class_create(THIS_MODULE, "char-dev"); 	
+	if(char_dev_sysfs_class == NULL) {
+		unregister_chrdev_region(first,1);
+		printk("class_create fail");
+		return -ENOMEM;
+	}
+
+	if(device_create(char_dev_sysfs_class, NULL, first, NULL, DEVICE_NAME) == NULL)
+	{
+	    class_destroy(char_dev_sysfs_class);
+	    unregister_chrdev_region(first,1);
+	    printk("device_create fail");
+	    return -ENOMEM;
+	}
+
+	char_dev_sysfs_cdev = cdev_alloc();
+	if(char_dev_sysfs_cdev == NULL) {
+		device_destroy(char_dev_sysfs_class, first);
+		class_destroy(char_dev_sysfs_class);
+		unregister_chrdev_region(first,1);		
+		printk(KERN_ERR "failed to alloc cdev\n");
+		return -ENOMEM;
+	}
+
+	cdev_init(char_dev_sysfs_cdev, &fops);
+
+	retval = cdev_add(char_dev_sysfs_cdev, first, 1);
+	if(retval < 0) {		
+		device_destroy(char_dev_sysfs_class, first);
+		class_destroy(char_dev_sysfs_class);
+		unregister_chrdev_region(first,1);
+		cdev_del(char_dev_sysfs_cdev);
+		printk(KERN_ERR "failed to add cdev\n");
+		return -EBUSY;
+	}
+
+	return 0;
+}
+
+static void __exit char_dev_sysfs_exit(void)
+{
+	device_destroy(char_dev_sysfs_class, first);
+	cdev_del(char_dev_sysfs_cdev);
+	class_destroy(char_dev_sysfs_class);
+	unregister_chrdev_region(first, 1);
+	
+	printk(KERN_INFO "module exit \n");
+}
+
+module_init(char_dev_sysfs_init);
+module_exit(char_dev_sysfs_exit);
+
+MODULE_LICENSE("GPL"); 
+MODULE_AUTHOR("Maeng"); 
+MODULE_DESCRIPTION("A character device module init");
+MODULE_VERSION("0.1"); 
+
